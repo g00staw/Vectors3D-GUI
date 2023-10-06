2789d4eb6edf8d267f9034674fc2ee6fa65c7db5
Konrad Pluta <konrad.k.j.pluta@gmail.com>
2023-10-06 22:02:05 +0200

    Stworzenie okna menu glownego

---
+++ src/MainMenu.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainMenu extends JFrame {
+    public MainMenu(){
+        setTitle("Vectors3D");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1920,1080);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
